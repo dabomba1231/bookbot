af78cade3dd89b3bd21ee9ae12d2987194f30f15
dabomba1231 <loganvannote@gmail.com>
2024-09-08 16:55:06 -0400

    added input option to use alternate documents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = input("Input path to book/document. ")
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     characters = get_character_count(text)
